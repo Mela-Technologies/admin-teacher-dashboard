408c54f233db115c2d0206d36f0fb433ac21a67f
YaredAlex <yareda25@gmail.com>
2025-04-26 20:39:56 +0530

    created logo component

---
+++ src/components/schoolLogo.tsx
@@ -0,0 +1,6 @@
+import schoolLogo from "../assets/images/school_logo.png";
+const SchoolLogo = () => {
+  return <img src={schoolLogo} alt="school image" width={50} height={50} />;
+};
+
+export default SchoolLogo;
