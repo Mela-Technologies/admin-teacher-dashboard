414b403bb685561c15623271e829620a05db8ddf
YaredAlex <yareda25@gmail.com>
2025-04-26 21:38:56 +0530

    added header

--- src/pages/authentication/loginPage.tsx
+++ src/pages/authentication/loginPage.tsx
@@ -1,66 +1,64 @@
-import { Form, Input, Button, Select, Card } from "antd";
+import { Form, Input, Button, Card, Layout, Select } from "antd";
 import useLogin from "../../hooks/useLogin";
-
-const { Option } = Select;
-
+import SchoolLogo from "../../components/schoolLogo";
+export type LoginProps = { role?: string; email?: string; password?: string };
 const LoginPage = () => {
   const [form] = Form.useForm();
   const { login, loading } = useLogin();
+  const { Header } = Layout;
 
-  const onFinish = (values) => {
+  const onFinish = (values: LoginProps) => {
+    console.log(values);
     login(values);
   };
 
   return (
-    <div className="flex justify-center items-center h-screen">
-      <Card className="w-full max-w-md shadow-xl rounded-xl">
-        <h2 className="text-2xl font-semibold text-center mb-4">Login</h2>
-        <Form
-          layout="vertical"
-          form={form}
-          onFinish={onFinish}
-          initialValues={{ role: "admin" }}
-        >
-          <Form.Item
-            name="role"
-            label="Login as"
-            rules={[{ required: true, message: "Please select a role" }]}
+    <Layout className="h-screen">
+      <Header>
+        <SchoolLogo />
+      </Header>
+      <div className="flex justify-center items-center h-full w-full">
+        <Card className="w-full max-w-md shadow-xl rounded-xl">
+          <h2 className="text-2xl font-semibold text-center mb-4">Login</h2>
+          <Form
+            layout="vertical"
+            form={form}
+            onFinish={onFinish}
+            initialValues={{ role: "admin" }}
           >
-            <Select>
-              <Option value="admin">Admin</Option>
-              <Option value="teacher">Teacher</Option>
-            </Select>
-          </Form.Item>
-
-          <Form.Item
-            name="email"
-            label="Email"
-            rules={[{ required: true, message: "Please enter your email" }]}
-          >
-            <Input />
-          </Form.Item>
-
-          <Form.Item
-            name="password"
-            label="Password"
-            rules={[{ required: true, message: "Please enter your password" }]}
-          >
-            <Input.Password />
-          </Form.Item>
-
-          <Form.Item>
-            <Button
-              type="primary"
-              htmlType="submit"
-              loading={loading}
-              className="w-full"
+            <Form.Item name={"role"} label="Role">
+              <Select>
+                <Select.Option value="admin">Admin</Select.Option>
+                <Select.Option value="teacher">Teacher</Select.Option>
+              </Select>
+            </Form.Item>
+            <Form.Item
+              name="email"
+              label="Email"
+              rules={[{ required: true, message: "Please enter your email" }]}
             >
-              Login
-            </Button>
-          </Form.Item>
-        </Form>
-      </Card>
-    </div>
+              <Input />
+            </Form.Item>
+
+            <Form.Item
+              name="password"
+              label="Password"
+              rules={[
+                { required: true, message: "Please enter your password" },
+              ]}
+            >
+              <Input.Password />
+            </Form.Item>
+
+            <Form.Item>
+              <Button type="primary" htmlType="submit" loading={loading} block>
+                Login
+              </Button>
+            </Form.Item>
+          </Form>
+        </Card>
+      </div>
+    </Layout>
   );
 };
 
