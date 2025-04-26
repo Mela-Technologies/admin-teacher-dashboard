d23fd565050ebc951dbd5eda855c0607009e966f
YaredAlex <yareda25@gmail.com>
2025-04-26 20:38:43 +0530

    modified dropdown

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,26 +1,40 @@
-import { Avatar, Dropdown, Menu } from "antd";
-import { UserOutlined, LogoutOutlined } from "@ant-design/icons";
+import { Avatar, Dropdown, MenuProps } from "antd";
+import {
+  UserOutlined,
+  LogoutOutlined,
+  SettingOutlined,
+} from "@ant-design/icons";
 
 const Header = () => {
   const user = {
-    name: "John Doe",
+    name: "Mela Tech",
     role: "admin",
   };
+  const items: MenuProps["items"] = [
+    {
+      key: "1",
+      label: "My Account",
+      icon: <UserOutlined />,
+    },
+    {
+      type: "divider",
+    },
 
-  const menu = (
-    <Menu>
-      <Menu.Item icon={<UserOutlined />} key="profile">
-        Profile
-      </Menu.Item>
-      <Menu.Item icon={<LogoutOutlined />} key="logout">
-        Logout
-      </Menu.Item>
-    </Menu>
-  );
+    {
+      key: "2",
+      label: "Settings",
+      icon: <SettingOutlined />,
+    },
+    {
+      key: "3",
+      label: "Logout",
+      icon: <LogoutOutlined />,
+    },
+  ];
 
   return (
     <div className="flex justify-between items-center h-full gap-4">
-      <Dropdown overlay={menu} placement="bottomRight">
+      <Dropdown menu={{ items }} placement="bottomRight">
         <div className="flex items-center gap-2 cursor-pointer">
           <Avatar icon={<UserOutlined />} />
           <span>{user.name}</span>
